6fce16d856756babbedaac673c84b900bdb627db
jhtacoma <j.hutchings@tacomaengineers.com>
2025-01-06 10:39:48 -0500

    Added one more button to panel

---
+++ LearnAPI.tab/Dev.panel/DevButton_4.pushbutton/script.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+__title__   = "TEMP button 4"
+__doc__     = """Version = 1.0
+Date    = 15.06.2024
+________________________________________________________________
+Description:
+
+________________________________________________________________
+Instructions:
+
+________________________________________________________________
+Author: Jamie Hutchings"""
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝
+#==================================================
+from Autodesk.Revit.DB import *
+
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝
+#==================================================
+app    = __revit__.Application
+uidoc  = __revit__.ActiveUIDocument
+doc    = __revit__.ActiveUIDocument.Document #type:Document
+
+
+# ╔╦╗╔═╗╦╔╗╔
+# ║║║╠═╣║║║║
+# ╩ ╩╩ ╩╩╝╚╝
+#==================================================
+# from Snippets._selection import get_selected_elements
+#
+# sel_el = get_selected_elements ()
+# sel_walls = get_selected_elements ([Wall, Floor])
+#
+# print (sel_el)
+# print (sel_walls)
+#
+#
+
+
+
+from Snippets._trace import trace
+
+
+a = 5
+b = "Johnny"
+trace ([a, b], 'bunch of stuff')
+trace (a, 'a')
+trace ()
+trace (b, 'b')
